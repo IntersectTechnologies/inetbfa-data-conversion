a3132c024bd9bee5f3579b92006b126e4c9a2ba0
Niel Swart <niel.swart@outlook.com>
2015-06-15 18:47:00 +0200

    Added envs module.

---
+++ envs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 20:17:57 2015
+
+@author: Niel
+"""
+from os import path
+
+root = path.join('C:\\', 'root')
+
+DL_PATH = path.join(root, 'data_downloads')
+MASTER_DATA_PATH = path.join(root, 'data')    
+
+DATA_PATH = path.join(root, 'intersect', 'data')
