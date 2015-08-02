e8ec0476b98401f6e758505ce00ad3dd5fa721f3
Niel Swart <niel.swart@outlook.com>
2015-08-02 21:03:31 +0200

    WIP: dataflow graph language for *financial* data processing

--- quantstrategies/quantstrategies/simple_test.py
+++ quantstrategies/quantstrategies/simple_test.py
@@ -1,6 +1,6 @@
 ﻿#...
 from datamanager import load
-from quantstrategies.strategies.strategy import Node
+from quantstrategies.strategies.strategy import *
 from quantstrategies.transforms import mean, momentum, log_returns, index_log_returns, movavg, max
 from functools import partial
 
@@ -9,8 +9,9 @@ long_movavg = partial(movavg, period=50)
 max20 = partial(max, period = 20)
 
 price_close = load.load_close()
-datan = Node(price_close.ix[-100:])
-
+datan = SourceNode(price_close.ix[-100:])
+reporter = SinkNode(name = 'StatsReport')
 do_stats = datan.filter(load.get_all_listed()).transform(log_returns).transform(index_log_returns).stats(movavg20 = short_movavg, movavg50 = long_movavg, max = max)
+stats = datan.stats(momentum = momentum, mean = mean).to_(reporter)
 
-datan.stats(momentum = momentum, mean = mean)
+reporter.report()
--- quantstrategies/quantstrategies/strategies/strategy.py
+++ quantstrategies/quantstrategies/strategies/strategy.py
@@ -6,37 +6,133 @@ Created on Sat May 23 15:22:03 2015
 """
 
 import pandas as pd
+from functools import partial
+from uuid import uuid4
 
-class Node:
-    def __init__(self, data):
+# TODO: create callback class / partial function that wraps the repetive logic of callbacks and node registration
+class Node(object):
+    def __init__(self, data=None, name = 'Node'):
         '''
         '''
+
+        self.name = name
+        self.guid = uuid4()
         self.data = data
+        self.callbacks = []
+        self.from_nodes = []
 
-    def _action_(self, **kwargs):
+    def __call__(self):
+        '''
+        '''
+
+        print 'Node ' + str(self.guid) + ' called...'
+
+        # wait until all from nodes have been called...
+        if len(self.callbacks) > 0:
+            for cb in self.callbacks:
+                cb()
+        else:
+            print('No callbacks registered...')
+
+    def to_(self, node):
+        '''
+        '''
+
+        def copy():
+            node.data = self.data.copy()
+            node()
+
+        self.callbacks.append(copy)
+        return self
+
+    def from_(self, *nodes):
+        '''
+        '''
+        
+        self.append(nodes);
+
+        
+class SourceNode(Node):
+    '''
+    '''
+
+    def __init__(self, data=None, name = 'SourceNode'):
+        '''
+        '''
+
+        super(SourceNode, self).__init__(data, name = 'SourceNode')
+
+    def transform(self, func):
+        
+        node = SourceNode()
+        def transformfunc():
+            print('Calling transform on ' + str(self.guid))
+            tmp = func(self.data)
+            node.data = tmp
+            node()
+
+        self.callbacks.append(transformfunc)
+        return node
+
+    def stats(self, **kwargs):
         '''
         Call a node with a named functions to apply to the data
         It will return a DataFrame object with the keys as index
         '''
-        tmp = {}
-        for k, func in kwargs.items():
-            tmp[k] = func(self.data)
+
+        node = SourceNode()
+        def statsfunc():
+            print('Calling stats on ' + str(self.guid))
+            tmp = {}
+            for k, func in kwargs.items():
+                tmp[k] = func(self.data)
         
-        return Node(pd.DataFrame.from_dict(tmp, orient='index'))
+            node.data = pd.DataFrame.from_dict(tmp, orient='index')
+            node()
 
-    def transform(self, func):
-        tmp = func(self.data)
-        return Node(tmp)
-
-    def stats(self, **kwargs):
-        return self._action_(**kwargs)
+        self.callbacks.append(statsfunc)
+        return node
 
     def filter(self, *args):
-        filtered = self.data.columns
 
-        for filter in args:
-            filtered = filtered.intersection(filter)
+        node = SourceNode()
+        node.from_nodes.append(self)
+        def filterfunc():
+            print('Calling filter on ' + str(self.guid))
+            filtered = self.data.columns
 
-        return Node(self.data[filtered])
-        
+            for filter in args:
+                filtered = filtered.intersection(filter)
 
+            node.data = self.data[filtered].copy()
+            node()
+
+        self.callbacks.append(filterfunc)
+        return node
+
+class StrategyNode(Node):
+    '''
+    '''
+
+    def __init__(self, data=None, name = 'StrategyNode'):
+        '''
+        '''
+
+        super(StrategyNode, self).__init__(data, name = 'StrategyNode')
+
+class SinkNode(Node):
+    '''
+    '''
+
+    def __init__(self, data=None, name = 'SinkNode'):
+        '''
+        '''
+
+        super(SinkNode, self).__init__(data, name = 'SinkNode')
+
+    def report(self):
+
+        def printer():
+            print(self.data)
+
+        self.callbacks.append(printer)
