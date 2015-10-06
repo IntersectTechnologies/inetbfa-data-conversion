b160609fc30c177762ce08e32780fe3c1ab55255
Niel Swart <niel.swart@vastech.co.za>
2015-10-06 09:21:46 +0200

    Refactored and fixed adjusted close calculation

--- datamanager/adjust.py
+++ datamanager/adjust.py
@@ -8,7 +8,30 @@ import pandas as pd
 from datamanager.datamodel import DataModel
 from datamanager.load import get_equities
 
-def calc_divmult(div, close):
+def __backwards_calc__(multiplier):
+    '''
+    Backwards calculate multipliers:
+    div 10: Mult10 = (1- div/price)
+    div 9 : Mult9 = (1-div/price)*Mult10
+    etc
+
+    return bmult: Series
+    '''
+    assert isinstance(multiplier, pd.Series)
+    # sort from newest to oldest
+    multiplier = multiplier.sort_index(ascending=False)
+    
+    # make a copy of the multiplier Series
+    mult = multiplier.copy()
+    for i, m in enumerate(multiplier):
+        if i == 0:
+            mult[i] = m
+        else:
+            mult[i] = m*mult[i-1]
+
+    return mult
+    
+def calc_dividend_multiplier(div, close):
     '''
     params:
     div - dividends of a single ticker : Seriess
@@ -20,30 +43,9 @@ def calc_divmult(div, close):
     assert isinstance(div, pd.Series)
     assert isinstance(close, pd.Series)
 
-    # calculate multiplier for each dividend payment
+    # calculate multiplier for each dividend payment and remove all the NaN to ease calculation
     mult = (1-(div/close)).dropna()
-    mult = mult.sort_index(ascending=False)
-
-    return mult
-
-def backwards_calc(multiplier):
-    '''
-    Backwards calculate multipliers:
-    div 10: Mult10 = (1- div/price)
-    div 9 : Mult9 = (1-div/price)*Mult10
-    etc
-
-    return bmult: dict
-    '''
-    assert isinstance(multiplier, pd.Series)
-    bmult = {}
-    for i, mult in enumerate(multiplier):
-        if i == 0:
-            bmult[i] = mult
-        else:
-            bmult[i] = mult*multiplier[i-1]
-
-    return bmult
+    return __backwards_calc__(mult)
 
 def calc_adj_close(closepath, divpath):
 
@@ -54,28 +56,25 @@ def calc_adj_close(closepath, divpath):
     divs = pd.read_csv(divpath, index_col = 0, parse_dates=True)
 
     # fillna with pad.
-    divmult = {}
+    divmult = pd.DataFrame()
     for ticker in divs.columns:
         # get the dividends and close of a single ticker and drop all NaN values
         tmp_div = divs[ticker].dropna()
         tmp_close = close[ticker].dropna()
+        
+        # now calculate the dividend multiplier for each ticker
+        tmp_mult = calc_dividend_multiplier(tmp_div, tmp_close)
+        divmult[ticker] = tmp_mult
 
-        tmp_mult = calc_divmult(tmp_div, tmp_close)
-        mult = backwards_calc(tmp_mult)
-        divmult[ticker] = pd.Series(mult.values(), tmp_mult.index)
-
+    # get the new index 
     startdate = pd.datetime(2000, 1 , 1).date()
     divm = DataModel.blank_ts_df(list(get_equities().index), startdate)
 
-    # ensure the integrity of the dataframe stays -- simplify algo
-    for k in divmult.keys():
-        divm[k] = divmult[k]
-
-        divm.set_value(divm.index[-1], k, 1.0)
+    # update the blank dataframe - expand to the actual index
+    divm.update(divmult) 
 
     # fillna with pad
-    dmult = divm.bfill()
-    adj_close = close * dmult;
+    adj_close = close * divm.bfill()
 
     # Save to file
     return adj_close
