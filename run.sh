b3986d712a0c841bd5d4655c2c21587c5b6fa81a
Niel Swart <niel.swart@outlook.com>
2015-10-12 20:20:16 +0200

    Updated run bash script

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 cd /c/root/intersect/datamanager
-echo "Running data tasks"
+echo "Updating data..."
 doit
 echo "Copying data..."
 cp -ruv /c/root/data/master/* /c/GDrive/NWU/data/
 cp -ruv /c/root/data/master/* /c/root/workspace/data
-echo "archiving data"
-tar -cvzf /c/root/backup/`date +%Y-%m-%d`.tar.gz /c/root/data/
+
 cd /c/root/intersect/quantstrategies
-echo "Creating Portfolios"
+echo "Creating Portfolios..."
 doit
-echo "Copying data"
+echo "Copying Portfolio data..."
 cp -ruv /c/root/data/models/* /c/GDrive/NWU/Portfolio
+
+echo "Archiving data..."
+tar -cvzf /c/root/backup/`date +%Y-%m-%d`.tar.gz /c/root/data/
+
+echo "Copying backup to NAS"
+cp -ruv /c/root/backup/`date +%Y-%m-%d`.tar.gz /z/backup/data/`date +%Y-%m-%d`.tar.gz 
