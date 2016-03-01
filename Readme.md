2c20b403064a1e575c6613c9cf108f960016b142
Niel Swart <niel.swart@outlook.com>
2016-03-01 17:46:10 +0200

    Updates to folder

--- Readme.md
+++ Readme.md
@@ -1,2 +1,14 @@
 # inetbfa-data-conversion
 Converts and merges data downloaded with the INETBFA Excel Add-In to csv formats
+
+## Data definitions etc:
+
+### Dividend Data
+
+**Dividend Declaration Date**
+
+**Dividend Ex Date**
+- The last date on which the investor is entitled to receive dividends
+
+Dividend Last Date to Register
+ - The 
---
+++ run.sh
@@ -0,0 +1,18 @@
+cd /c/root/intersect/datamanager
+echo "Updating data..."
+doit
+echo "Copying data..."
+cp -ruv /c/root/data/merged/* /c/GDrive/NWU/data/
+cp -ruv /c/root/data/merged/* /c/root/workspace/data
+
+cd /c/root/intersect/quantstrategies
+echo "Creating Portfolios..."
+doit
+echo "Copying Portfolio data..."
+cp -ruv /c/root/data/models/NWU*.xlsx /c/GDrive/NWU/Portfolios
+
+echo "Archiving data..."
+tar -cvzf /c/root/backup/`date +%Y-%m-%d`.tar.gz /c/root/data/
+
+echo "Copying backup to NAS"
+cp -ruv /c/root/backup/`date +%Y-%m-%d`.tar.gz /z/backup/data/`date +%Y-%m-%d`.tar.gz 
