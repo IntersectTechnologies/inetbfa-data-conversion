e769e9256114c9444863adce4a54f82767609807
Niel Swart <niel.swart@outlook.com>
2016-03-04 20:28:06 +0200

    Removed conversion scripts

--- convert.sh
+++
@@ -1,5 +0,0 @@
-mkdir /c/root/data/converted/tickers
-cd /c/root/intersect/datamanager
-doit convert* swap
-cd /c/root/data/converted/tickers
-csvstack --filename -n tickers *.csv > all_jse_equity_data.csv
