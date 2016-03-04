abc915a5471ee6248c0154999ff5e3e7161b4d75
Niel Swart <niel.swart@outlook.com>
2016-03-04 20:28:30 +0200

    Added tests and ci config files

---
+++ test/calculation_test.py
@@ -0,0 +1,17 @@
+﻿from datamanager.envs import *
+from datamanager.adjust import *
+import pandas as pd
+
+def test_backwards_calc():
+    '''
+    '''
+
+def test_adjusted_close():
+    # Import closing price data with pandas
+    cp = path.join(MERGED_PATH, "Close.csv")
+    dp = path.join(MERGED_PATH, "Dividend Ex Date.csv")
+    close = pd.read_csv(cp, index_col = 0, parse_dates=True)
+    divs = pd.read_csv(dp, index_col = 0, parse_dates=True)
+
+    assert isinstance(close, pd.DataFrame)
+    assert isinstance(divs, pd.DataFrame)
