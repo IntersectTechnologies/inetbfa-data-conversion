6702caf2ce7f75f2e7d8685fc9f5887bd812b975
Niel Swart <niel.swart@outlook.com>
2016-03-05 16:45:36 +0200

    Fixed file paths

--- datamanager/envs.py
+++ datamanager/envs.py
@@ -6,23 +6,9 @@ Created on Tue May 19 20:17:57 2015
 """
 from os import path
 
-root = path.join('C:\\', 'root')
-DATA_ROOT = path.join(root, 'data')
-
+dir = path.dirname(__file__)
+DATA_ROOT = path.join(dir , '..')
 DL_PATH = path.join(DATA_ROOT, 'downloads')
 MASTER_DATA_PATH = path.join(DATA_ROOT, 'master')    
-
-BACKUP_PATH = path.join(root, 'backup')
-DATA_PATH = path.join(root, 'workspace', 'data')
 CONVERT_PATH = path.join(DATA_ROOT, 'converted')
 MERGED_PATH = path.join(DATA_ROOT, 'merged')
-PORTFOLIO_PATH = path.join(DATA_ROOT, 'portfolios')
-MODEL_PATH = path.join(DATA_ROOT, 'models')
-INDICES_PATH = path.join(DATA_ROOT, 'indices')
-TRANSACT_PATH = path.join(DATA_ROOT, 'transactions')
-
-'''
-Directory structure:
-    root/{exchange}/{security-type}/{frequency}
-    
-'''
