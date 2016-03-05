d9019bde4156119ede519a1f4672eb0a6ca93ea5
Niel Swart <niel.swart@outlook.com>
2016-03-05 18:45:51 +0200

    Removed dependency on data in tests

---
+++ initialise.sh
@@ -0,0 +1,4 @@
+mkdir -p master
+mkdir -p merged
+mkdir -p converted
+mkdir -p downloads
--- test/calculation_test.py
+++ test/calculation_test.py
@@ -7,11 +7,6 @@ def test_backwards_calc():
     '''
 
 def test_adjusted_close():
-    # Import closing price data with pandas
-    cp = path.join(MERGED_PATH, "Close.csv")
-    dp = path.join(MERGED_PATH, "Dividend Ex Date.csv")
-    close = pd.read_csv(cp, index_col = 0, parse_dates=True)
-    divs = pd.read_csv(dp, index_col = 0, parse_dates=True)
-
-    assert isinstance(close, pd.DataFrame)
-    assert isinstance(divs, pd.DataFrame)
+    '''
+    test should not be dependent on data in files
+    '''
