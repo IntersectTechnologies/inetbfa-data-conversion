198c5d2663716266e642d672ebd0da818a56dc61
Niel Swart <nielswart@intersect-technologies.com>
2016-03-07 08:10:42 +0200

    Added new transformation module

---
+++ datamanager/transforms.py
@@ -0,0 +1,12 @@
+'''
+This module contains transformation functions for time series data based on pandas DataFrames
+'''
+
+def resample_monthly(data):
+    '''
+    Resample the data to a monthly frequency using a specific aggregation function
+    '''
+
+def moving_avg(data, days):
+    '''
+    '''
