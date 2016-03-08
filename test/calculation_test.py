ef6669a86d8208e574247770c5ca9c88bff6507f
Niel Swart <nielswart@intersect-technologies.com>
2016-03-08 17:56:25 +0200

    Fixed issue for python 3.4 build

--- test/calculation_test.py
+++ test/calculation_test.py
@@ -18,7 +18,7 @@ def test_book2market():
     '''
     '''
     
-    dt_ix = [dt.date(2016, 01, d) for d in range(1,31)]
+    dt_ix = [dt.date(2016, 1, d) for d in range(1,31)]
     data = data = [ random.random()*10 if (d > 15 and d < 20 or (d < 10 and d > 5)) else np.NaN for d in range(1,31)]
     
     df = pd.DataFrame(data, index = dt_ix, columns = ['Values'])
