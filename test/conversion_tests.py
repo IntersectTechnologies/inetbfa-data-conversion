a91c3c95993c75c8e672393c8ff0488865d0cfe7
Niel Swart <nielswart@intersect-technologies.com>
2016-03-28 11:21:08 +0200

    Fixed test

--- test/conversion_tests.py
+++ test/conversion_tests.py
@@ -3,7 +3,6 @@ from mock_data import TESTDATA
 import numpy as np
 
 def test_last_date_of_conversion():
-    
-    select = TESTDATA.index[TESTDATA.index > np.datetime64(last_month_end())]
+    select = TESTDATA.index[TESTDATA.index.values.astype('datetime64[D]') > np.datetime64(last_month_end())]
 
     assert len(select) == 0
